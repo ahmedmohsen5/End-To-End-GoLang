8709f4e83a6aef1c86c2d27b2a67fd5d495408de
ahmed5555555555 <a.khalefa555@gmail.com>
2024-01-01 23:43:35 +0200

    upload the terraform fiels

---
+++ terraform/vpc.tf
@@ -0,0 +1,33 @@
+module "vpc" {
+    source = "terraform-aws-modules/vpc/aws"
+    version = "~> 3.0"
+    name = "${locals.cluser_name}-vpc"
+    cidr = var.main_network_block
+    azs = data.aws_availability_zones.aws_available.names
+
+    private_subnets = ["10.0.0.0/20", "10.0.16.0/20"]
+
+    public_subnets = ["10.0.128.0/20", "10.0.144.0/20"]
+
+    enable_nat_gateway = true
+    single_nat_gateway = true
+    one_nat_gateway_per_az = false
+    create_database_subnet_group = false
+    enable_dns_hostnames = true
+    reuse_nat_ips = true
+    external_nat_ip_ids = [aws_eip.nat_gw_elastic_ip.id]
+
+    public_acl_tags = {
+        "kubernetes.io/cluster/${locals.eks_cluster_id}" = "shared"
+        "kubernetes.io/role/elb"                      = "1"
+    }
+    private_acl_tags = {
+        "kubernetes.io/cluster/${locals.eks_cluster_id}" = "shared"
+        "kubernetes.io/role/internal-elb"             = "1"
+    }
+    tags = {
+        Environment = "test"
+    }
+
+
+}
