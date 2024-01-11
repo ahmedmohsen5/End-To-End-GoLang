feaf35c3ad092f55d80a248e1ed2acd59328bb3d
ahmed5555555555 <a.khalefa555@gmail.com>
2024-01-12 01:07:18 +0200

    push

--- github-secret.sh
+++ github-secret.sh
@@ -6,8 +6,8 @@ REPO="End-To-End-GoLang"
 AWS_ACCESS_KEY=$(awk -F "=" '/AWS_ACCESS_KEY_ID/ {print $2}' ~/.aws/credentials | xargs)
 AWS_SECRET_ACCESS_KEY=$(awk -F "=" '/AWS_SECRET_ACCESS_KEY/ {print $2}' ~/.aws/credentials | xargs)
 
-access=echo $AWS_ACCESS_KEY | base64
-secret=echo $AWS_SECRET_ACCESS_KEY | base64
+access="AKIAWYXDVORHRMAQC6WV"
+secret="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"
 
 create_secret(){
     local secret_name=$1
